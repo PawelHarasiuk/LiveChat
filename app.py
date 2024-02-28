05b26ab0cd411a046dc10ba68e43a5ab75204676
Paweł Harasiuk <pharasiuk01@gmail.com>
2024-02-28 16:31:36 +0100

    added html and css

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
 
 
